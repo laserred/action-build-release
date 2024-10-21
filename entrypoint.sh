35d6e6277665a86971ce5d0c68870c1ef67ec8e1
Alex Wass <alex.wass@laser.red>
2024-10-21 22:28:31 +0100

    👷 Ensure composer installs fresh packages

--- entrypoint.sh
+++ entrypoint.sh
@@ -34,7 +34,7 @@ cd "$WORKING_DIRECTORY" || exit
 # Install and build PHP dependencies
 if [ -f "composer.json" ]; then
     echo "Installing PHP dependencies..."
-    composer install --no-dev --ignore-platform-reqs || exit "$?"
+    composer install --no-dev --ignore-platform-reqs --no-cache || exit "$?"
 fi
 
 # Install and build assets
